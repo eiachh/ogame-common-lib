3fd9cf7c5388516b32b12b993f2ecaab290d97f1
Adam Sranko <srankoadam@gmail.com>
2022-11-21 23:03:20 +0100

    Added repeating Timer to utilities

--- utilities.py
+++ utilities.py
@@ -1,4 +1,30 @@
+from threading import Timer
 from common_lib.const import constants
+
+class RepeatedTimer(object):
+    def __init__(self, intervalFunc, function, *args, **kwargs):
+        self._timer     = None
+        self.getIntervalFunc   = intervalFunc
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.getIntervalFunc(), self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
 
 class utilities:
     def isPrerequisiteMet(ogameID, requestData):
